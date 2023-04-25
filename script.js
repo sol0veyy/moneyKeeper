703a00a827a8bcb15dbcb43e3e1b05b1508eaae3
sol0vey <solovevdaniil03@mail.ru>
2023-04-25 15:02:09 +0300

    change script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-let money;
-let time;
-
-money = prompt("Ваш бюджет на месяц?", "");
-time = prompt("Введите дату в формате YYYY-MM-DD", "");
-let answer1 = prompt("Введите обязательную статью расходов в этом месяце", "")
-let answer2 = prompt("Во сколько обойдется", "")
+let money = prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
     budget: money,
-    timeData: time,
-    expenses: {
-        answer1:answer2,
-    },
+    expenses: {},
     optionalExpenses: {},
     income: [],
+    timeData: time,
     savings: false
-}
+};
 
-alert("Бюджет на 1 день: " + money / 30 + " руб.")
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", "");
+let a2 = prompt("Во сколько обойдется?", "");
+let a3 = prompt("Введите обязательную статью расходов в этом месяце", "");
+let a4 = prompt("Во сколько обойдется?", "");
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
